55bf798207077792eadd1b66a354fb41a7e40704
2013-20141207ZL\Administrator <>
2021-03-24 13:22:25 +0800

    test

--- test/go_test/example_test.go
+++ test/go_test/example_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/levigross/grequests"
@@ -10,6 +11,8 @@ import (
 func TestExample(t *testing.T) {
 	resp, err := grequests.Get("http://httpbin.org/get", nil)
 	defer resp.Close()
+	fmt.Println(resp.String())
+	fmt.Println(resp.RawResponse)
 	assert.Nil(t, err)
 	assert.Equal(t, 200, resp.StatusCode, "expected 200 status code")
 }
